7524d2f286259516233f402d46fe2e728e5dcb20
bindingofisaac <bindingofisaacs@gmail.com>
2015-06-05 19:08:09 +0530

    tamperscript

---
+++ tamperscript.js
@@ -0,0 +1,16 @@
+==UserScript==
+// @name         My Fancy New Userscript
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        http://www.flipkart.com/
+// @grant        none
+// ==/UserScript==
+var d = document.createElement("script");
+var e = document.createElement("div");
+var f = document.createTextNode("abC");
+e.appendChild(f);
+d.type = "text/javascript";
+d.src = "https://raw.githubusercontent.com/ssundarraj/fk-concierge/master/main.js";
+document.getElementsByTagName("body")[0].appendChild(d);
