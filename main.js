2f639cdf1778b9cd4cd1a4c922e3a4c72fc7d063
Gokul Srinivas <gokusrinivas@gmail.com>
2015-06-05 19:16:09 +0530

    changed main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-/*var recognition = new webkitSpeechRecognition();
+var d = document.createElement("script");
+var e = document.createElement("div");
+e.id = "result";
+d.type = "text/javascript";
+d.src = "https://rawgit.com/ssundarraj/fk-concierge/master/main.js";
+document.getElementsByTagName("body")[0].appendChild(d);
+document.getElementsByTagName("body")[0].appendChild(e);
+
+
+
+var recognition = new webkitSpeechRecognition();
 recognition.onresult = function(event) {
     console.log(event);
     var a = "";
@@ -8,5 +18,3 @@ recognition.onresult = function(event) {
     document.getElementById('result').innerHTML = parseString(a);
     // Add processing code here
 }
-*/
-alert();
