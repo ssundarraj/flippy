f41bc83b84ccb26f03d57dbe3d5759b71e3e7095
bindingofisaac <bindingofisaacs@gmail.com>
2015-06-05 18:57:27 +0530

    exp

---
+++ main.js
@@ -0,0 +1,12 @@
+/*var recognition = new webkitSpeechRecognition();
+recognition.onresult = function(event) {
+    console.log(event);
+    var a = "";
+    for(var i=0; i<event.results.length; i++){
+        a = a + event.results[i][0].transcript
+    }
+    document.getElementById('result').innerHTML = parseString(a);
+    // Add processing code here
+}
+*/
+alert();
