c5973b793f8c1e508e3057789ffd74d8cf7d7d46
bindingofisaac <bindingofisaacs@gmail.com>
2015-06-05 16:40:56 +0530

    fixed bug in parser.js

--- parser.js
+++ parser.js
@@ -7,20 +7,18 @@ function parseString(query)
 		// search code
 		search_string = query.replace("search ","");
 		console.log(search_string);
-		return query;
 	}
 	else if(query_arr[0] == "include")
 	{
 		// include code	
 		search_string = query.replace("include ","");
 		console.log(search_string);
-		return query;
 	}
 	else if(query_arr[0] == "exclude")
 	{
 		// exclude code
 		search_string = query.replace("exclude ","");
 		console.log(search_string);
-		return query;
 	}
+  return search_string;
 }
